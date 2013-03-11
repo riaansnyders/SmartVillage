4a9c11bb68463752255c38a46e2cba1314410831
riaansnyders <riaan@elevato.co.za>
2013-03-11 14:32:26 +0200

    Changeset 7
    Continued Endpoint integration and finshed security cache clearing
    functionality (SQL Server Job - Architecture change).

--- SmartCloud/Server/Native/Adapter/Program.cs
+++ SmartCloud/Server/Native/Adapter/Program.cs
@@ -18,7 +18,43 @@
     {
         public static void Main(string[] args)
         {
+            try
+            {
+                if (args.Length > 0)
+                {
+                    string activatedEndpoint = args[0];
+                    string loadId = args[1];
+                    string token = args[2];
 
+                    switch (activatedEndpoint)
+                    {
+                        case "AddSwitch":
+                        break;
+                        default:
+                        throw new ArgumentOutOfRangeException("The defined action or end point has not been found or is unavailable. Please try again.");
+                        break;
+                    }
+
+
+                }
+                else
+                {
+                    throw new ArgumentNullException("No parameters defined for provided action or end point!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(HandleException(ex));
+            }
         }
+
+        #region Exception Handling
+        private static string HandleException(Exception ex)
+        {
+            string json = "{ status: \"500\", identifier: {0}, exception: \"{1}\"}";
+
+            return string.Format(json, System.Guid.NewGuid().ToString(), ex.Message);
+        }
+        #endregion
     }
 }
